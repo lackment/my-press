059a5fa41be9f6b715bd4bc6b6bd9e995943c975
linyi <969610745@qq.com>
2023-03-08 00:46:09 +0800

    first commit

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,7 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:mqyqingfeng/learn-typescript.git master:gh-pages
+
+git push -f git@github.com:lackment/my-press.git.git master:gh-pages
 
 cd -
